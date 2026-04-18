f80bf9bc806a5bc60eb36496186696dc53d2ad84
sufyan-github <abusufyan.cse20@gmail.com>
2026-04-19 03:11:19 +0600

    add home and user route

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
+	// Home
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Go API is running ",
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Welcome to API",
+		})
+	})
+
+	// Users
+	r.GET("/users", func(c *gin.Context) {
+		users := []string{"Sufyan", "John", "Alice"}
+
+		c.JSON(http.StatusOK, gin.H{
+			"users": users,
 		})
 	})
 
